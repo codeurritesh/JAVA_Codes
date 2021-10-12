5b46741309045a81be89b991f354c606fa197f50
techieritesh <73749372+techieritesh@users.noreply.github.com>
2021-10-12 16:20:47 +0530

    aggregation

---
+++ aggregation.java
@@ -0,0 +1,24 @@
+class aggregation{  
+ int square(int n){  
+  return n*n;  
+ }  
+}  
+  
+class Circle{  
+ aggregation op;//aggregation  
+ double pi=3.14;  
+    
+ double area(int radius){  
+   op=new aggregation();  
+   int rsquare=op.square(radius);//code reusability (i.e. delegates the method call).  
+   return pi*rsquare;  
+ }  
+  
+     
+    
+ public static void main(String args[]){  
+   Circle c=new Circle();  
+   double result=c.area(5);  
+   System.out.println(result);  
+ }  
+}  
